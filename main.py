6dfa1e8121f861e3910867e628979d9d303b72a3
morganE257 <morganeggert@gmail.com>
2025-09-15 02:12:04 +0000

    Added in the rest of the base chatbot code

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-print("Welcome to the _____ chatbot!")
+import random
+
+loop_program =  True
+
+print("Welcome to the Elite 101 chatbot!")
 name = input("What is your name? ")
-age = input(f"Hello {name}, how old are you? ")
+age = int(input(f"Hello {name}, how old are you? "))
+if age <= 15:
+    print(f"Oh, {age}, to be young again! What can I do for you? ")
+else:
+    print(f"{age}, a wonderful age to be! What can I do for you? ")
+
+
+def options():
+    print("\n Please chose from the following options: ")
+    print("1. Placeholder Option 1")
+    print("2. Placeholder Option 2")
+    print("3. Placeholder Option 3")
+    print("4. Exit the conversation")
+
+def user_selection():
+    in_use = True
+    user_choice = int(input("Enter a number 1-4: "))
+    if user_choice == 1:
+        print("This is the first placeholder.")
+    elif user_choice == 2:
+        print("This is the second placeholder.")
+    elif user_choice == 3:
+        print("This is the third placeholder.")
+    elif user_choice == 4:
+        print("Exiting the program, thank you for visiting!")
+        in_use = False
+    else:
+        print("Sorry, that wasn't a valid option, please choose again.")
+    return in_use
+
+while loop_program:
+    options()
+    loop_program = user_selection()
